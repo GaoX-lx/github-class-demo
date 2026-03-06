e6779728cd92f2dbddbe7652c54eb056b92f0a64
GaoX-lx <GX2078712804@outlook.com>
2026-03-06 15:11:35 +0800

    feat: 为C语言程序新增键盘输入功能，更新README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # github-class-demo
 这是一个GitHub课堂演示项目，用于练习Git操作。
 当前程序包含ex01文件夹下的C语言HelloWorld程序。
+##更新日志
+-2026-03-06：为C语言HelloWorld程序新增键盘输入功能，可读取并显示用户输入内容
--- ex01/HelloWorld.c
+++ ex01/HelloWorld.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
-int main()
-{
-    printf("Hello World\n");
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    char input_content[100]; // 定义数组存输入内容
+    printf("请输入你想显示的内容：");
+    scanf("%s", input_content); // 读取键盘输入
+    printf("你输入的内容是：%s\n", input_content); // 输出输入内容
     return 0;
 }
