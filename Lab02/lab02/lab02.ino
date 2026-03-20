c4ff0218e37878043213d17359ca1675b231367b
GaoX-lx <GX2078712804@outlook.com>
2026-03-20 18:46:55 +0800

    两个灯闪烁

--- Lab02/lab02/lab02.ino
+++ Lab02/lab02/lab02.ino
@@ -1,25 +1,20 @@
 // 定义板载LED引脚，避免使用"魔法数字"
 #define LED_PIN_R 26
 #define LED_PIN_Y 25
-#define LED_PIN_G 33
-
 void setup() {
   // 初始化串口通信
   Serial.begin(115200);
   // 初始化板载LED引脚为输出模式
   pinMode(LED_PIN_R, OUTPUT); 
   pinMode(LED_PIN_Y, OUTPUT);
-  pinMode(LED_PIN_G, OUTPUT);
-}
+  }
 
 void loop() {
   Serial.println("Hello ESP32!");
   digitalWrite(LED_PIN_R, HIGH);   // 点亮LED
   digitalWrite(LED_PIN_Y, HIGH);   // 点亮LED
-  digitalWrite(LED_PIN_G, HIGH);   // 点亮LED
   delay(1000);                   // 等待1秒
   digitalWrite(LED_PIN_R, LOW);    // 熄灭LED
   digitalWrite(LED_PIN_Y, LOW);    // 熄灭LED
-  digitalWrite(LED_PIN_G, LOW);    // 熄灭LED
   delay(1000);              // 等待1秒
 }
